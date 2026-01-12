8e04cd6ee962480cb65a1e59f8015ed4c1fb1c25
timothymulatya <timothymulatya@gmail.com>
2026-01-12 15:18:45 +0300

    Implement Magazine class with all required methods

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -92,18 +92,58 @@ class Author:
         return list(categories)
 
 class Magazine:
+    all = []
+    
     def __init__(self, name, category):
         self.name = name
         self.category = category
-
+        self._articles = []
+        Magazine.all.append(self)
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string")
+        if not (2 <= len(name) <= 16):
+            raise ValueError("Name must be between 2 and 16 characters")
+        self._name = name
+    
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, category):
+        if not isinstance(category, str):
+            raise TypeError("Category must be a string")
+        if len(category) == 0:
+            raise ValueError("Category must be longer than 0 characters")
+        self._category = category
+    
     def articles(self):
-        pass
-
+        return self._articles
+    
     def contributors(self):
-        pass
-
+        return list({article.author for article in self._articles})
+    
     def article_titles(self):
-        pass
-
+        if not self._articles:
+            return None
+        return [article.title for article in self._articles]
+    
     def contributing_authors(self):
-        pass
+        author_counts = {}
+        for article in self._articles:
+            author_counts[article.author] = author_counts.get(article.author, 0) + 1
+        result = [author for author, count in author_counts.items() if count > 2]
+        return result if result else None
+    
+    @classmethod
+    def top_publisher(cls):
+        if not cls.all:
+            return None
+        return max(cls.all, key=lambda mag: len(mag.articles()))
